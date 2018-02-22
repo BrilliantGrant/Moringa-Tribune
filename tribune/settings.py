a07bdd350c464fc945c4bcc9b8a04710d70362cc
bri <briegrant416@gmail.com>
2018-02-22 19:17:14 +0300

    db config

--- tribune/settings.py
+++ tribune/settings.py
@@ -14,7 +14,6 @@ import os
 
 import dj_database_url
 
-db_from_env= dj_database_url.config(conn_max_age=500)
 
 from decouple import config
 
@@ -96,8 +95,8 @@ DATABASES = {
     }
 }
 
-
-# DATABASES['default'].update(db_from_env),
+db_from_env= dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env),
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
